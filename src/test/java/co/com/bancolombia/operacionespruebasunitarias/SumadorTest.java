21b627b9afa1ab42cfbf4d5522b51d36fc43042b
Diego Alexander Meneses <diemenes@bancolombia.com.co>
2022-10-20 08:21:01 -0500

    Clase inicial

--- src/test/java/co/com/bancolombia/operacionespruebasunitarias/SumadorTest.java
+++ src/test/java/co/com/bancolombia/operacionespruebasunitarias/SumadorTest.java
@@ -6,5 +6,22 @@ import static org.junit.Assert.assertEquals;
 
 public class SumadorTest {
 
+    @Test
+    public void sumarDosNumeros() {
+        //  Arrange
+        Sumador sumador = new Sumador();
+        double param1 = 4;
+        double param2 = 2.5;
+        double resultadoEsperado = 6.5;
+
+        //  Act
+        double resultado = sumador.sumar(param1, param2);
+
+        //  Assert
+        assertEquals(resultado, resultadoEsperado, 0D);
+
+    }
+
+
 
 }
